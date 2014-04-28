4ea4b0a337cba4bf4e2de58a7c99767e7fc2c703
Krzysztof Ołdakowski <krz.oldakowski@gmail.com>
2014-04-28 21:11:23 +0200

    Added 'blogger' rails project

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,23 @@
+class TagsController < ApplicationController
+
+def index 
+  @tags = Tag.all
+end
+
+def show
+  @tag = Tag.find(params[:id])
+end
+
+def edit
+  @tag = Tag.find(params[:id])
+end
+
+def destroy
+  @tag = Tag.find(params[:id])
+  @tag.destroy
+
+  flash.notice = "Tag '#{ @tag.name }' destroyed."
+
+  redirect_to tags_path
+end
+end
---
+++ blogger/config/routes.rb
@@ -0,0 +1,8 @@
+Blogger::Application.routes.draw do
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+
+end
